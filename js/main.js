a167354192e201af5d6929aa51db55f57bc8290d
dekysurahman <deky.surahman@gmail.com>
2022-01-25 16:48:43 +0700

    Update content and add js file

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const photo = document.querySelector(".photo");
+const name1 = document.querySelector(".name");
+const identity = document.querySelector(".identity");
+const address = document.querySelector(".address");
+
+photo.classList.add("anim");
+name1.classList.add("anim");
+identity.classList.add("anim");
+address.classList.add("anim");
