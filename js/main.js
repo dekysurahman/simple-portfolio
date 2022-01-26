87bf9bed9ea0b5992637820a3729be48031c0896
dekysurahman <deky.surahman@gmail.com>
2022-01-26 22:05:42 +0700

    Change header style

--- js/main.js
+++
@@ -1,9 +0,0 @@
-const photo = document.querySelector(".photo");
-const name1 = document.querySelector(".name");
-const identity = document.querySelector(".identity");
-const address = document.querySelector(".address");
-
-photo.classList.add("anim");
-name1.classList.add("anim");
-identity.classList.add("anim");
-address.classList.add("anim");
